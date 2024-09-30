346985c8e256cfba20eeaf6b1d4480296fbed1cb
KSburna <kelvinfatumoju@gmail.com>
2024-09-30 18:06:14 +0530

    add book search functionality

--- my_book_manager/views.py
+++ my_book_manager/views.py
@@ -4,46 +4,60 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login
 from .forms import CustomAuthUserCreationForm, CustomAuthUserLoginForm, UserDetailsForm
 
+GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes"
+
 
 # Home view with search functionality
 @login_required
 def home_view(request):
-    context = {}
-    return render(request, 'book/home.html', context)
+    query = request.GET.get("q")
+    books = []
+
+    if query:
+        response = requests.get(GOOGLE_BOOKS_API_URL, params={"q": query})
+        if response.status_code == 200:
+            data = response.json()
+            books = data.get("items", [])
+
+    if query is None:
+        query = ""
+    context = {"books": books, "query": query}
+    return render(request, "book/home.html", context)
+
 
 def signup_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomAuthUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('home')
+            return redirect("home")
     else:
         form = CustomAuthUserCreationForm()
-    return render(request, 'book/signup.html', {'form': form})
+    return render(request, "book/signup.html", {"form": form})
 
 
 def login_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomAuthUserLoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
-            return redirect('home')
+            return redirect("home")
     else:
         form = CustomAuthUserLoginForm()
-    return render(request, 'book/login.html', {'form': form})
+    return render(request, "book/login.html", {"form": form})
 
 
 @login_required
 def change_user_details(request):
     user = request.user
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserDetailsForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect("home")
     else:
         form = UserDetailsForm(instance=user)
 
-    return render(request, 'book/change_user_details.html', {'form': form})
+    return render(request, "book/change_user_details.html", {"form": form})
